fe0800956b4df50dd70d2f17d9ac922da579f156
marialuciafragoso <mllf.devtech@gmail.com>
2026-03-26 20:03:05 -0300

    Adicionado exercicio 10

--- exercicios.py
+++ exercicios.py
@@ -1,16 +1,16 @@
 # #### Inteiros (`int`)
 
 # 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
-numero_1 = int(input("Digite o primeiro número inteiro: "))
-numero_2 = int(input("Digite o segundo número inteiro: "))
-print (f"A soma de {numero_1} e {numero_2} é igual a {numero_1 + numero_2}")
+# numero_1 = int(input("Digite o primeiro número inteiro: "))
+# numero_2 = int(input("Digite o segundo número inteiro: "))
+# print (f"A soma de {numero_1} e {numero_2} é igual a {numero_1 + numero_2}")
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
-numero_1 = int(input( "Digite um numero inteiro:"))
-numero_2 = int(input("Digite outro numero inteiro:"))
-divisao_inteira = numero_1 // numero_2
-print(f"A divisão de {numero_1} por {numero_2} é igual a {divisao_inteira}")
+#numero_1 = int(input( "Digite um numero inteiro:"))
+#numero_2 = int(input("Digite outro numero inteiro:"))
+#divisao_inteira = numero_1 // numero_2
+#print(f"A divisão de {numero_1} por {numero_2} é igual a {divisao_inteira}")
 
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
 
@@ -20,9 +20,15 @@ print(f"A divisão de {numero_1} por {numero_2} é igual a {divisao_inteira}")
 # 8. Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
 # 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
 # 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
+import math
+
+
+raio_do_circulo = float(input("Digite o raio do círculo: "))
+area_circulo = math.pi * raio_do_circulo**2
+print(f"{area_circulo: .2f}")
+print(f"a área do círculo é igual a {area_circulo: .2f}")
 
 # #### Strings (`str`)
-
 # 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
