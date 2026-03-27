5c42bbf157809de32a1c7ef9151b422ce3574f43
marialuciafragoso <mllf.devtech@gmail.com>
2026-03-26 21:04:22 -0300

    Adicionado exercicio 14

--- exercicios.py
+++ exercicios.py
@@ -7,10 +7,12 @@
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+
 #numero_1 = int(input( "Digite um numero inteiro:"))
 #numero_2 = int(input("Digite outro numero inteiro:"))
 #divisao_inteira = numero_1 // numero_2
 #print(f"A divisão de {numero_1} por {numero_2} é igual a {divisao_inteira}")
+#print("Não é possível realizar essa operação!")
 
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
 
@@ -23,16 +25,24 @@
 import math
 
 
-raio_do_circulo = float(input("Digite o raio do círculo: "))
-area_circulo = math.pi * raio_do_circulo**2
-print(f"{area_circulo: .2f}")
-print(f"a área do círculo é igual a {area_circulo: .2f}")
+#raio_do_circulo = float(input("Digite o raio do círculo: "))
+#area_circulo = math.pi * raio_do_circulo**2
+#print(f"{area_circulo: .2f}")
+#print(f"a área do círculo é igual a {area_circulo: .2f}")
 
 # #### Strings (`str`)
 # 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
+data = str(input("Digite uma data no modelo dd/mm/aaaa: "))
+dia, mes, ano = data.split("/")
+print(f"Dia é {dia}")
+print (f"Mês é {mes}")
+print (f"Ano é {ano}")
+
+
+
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
 
 # #### Booleanos (`bool`)
