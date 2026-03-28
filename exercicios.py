2baa8ae1c3d2dff1e5f535060caa8faea59d0f45
marialuciafragoso <mllf.devtech@gmail.com>
2026-03-28 19:24:05 -0300

    Primeiro bloco de exercíios completo

--- exercicios.py
+++ exercicios.py
@@ -1,20 +1,47 @@
 # #### Inteiros (`int`)
 
 # 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
-# numero_1 = int(input("Digite o primeiro número inteiro: "))
-# numero_2 = int(input("Digite o segundo número inteiro: "))
-# print (f"A soma de {numero_1} e {numero_2} é igual a {numero_1 + numero_2}")
-# 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
-# 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
-# 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+try:
+    numero_1 = int(input("Digite o primeiro número inteiro: "))
+    numero_2 = int(input("Digite o segundo número inteiro: "))
+    print (f"A soma de {numero_1} e {numero_2} é igual a {numero_1 + numero_2}")
+except:
+    print("Insira um número inteiro")
 
-#numero_1 = int(input( "Digite um numero inteiro:"))
-#numero_2 = int(input("Digite outro numero inteiro:"))
-#divisao_inteira = numero_1 // numero_2
-#print(f"A divisão de {numero_1} por {numero_2} é igual a {divisao_inteira}")
-#print("Não é possível realizar essa operação!")
+# 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+try:
+    num_usuario = int(input("Digite um número inteiro: "))
+    resto_divisao = num_usuario % 5
+    print(f"O resto da divisão do número {num_usuario} por 5 é {resto_divisao}")
+except:
+    print("Insira um número inteiro")
+
+
+# 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+try:
+    num_1 = int(input("Digite um número: "))
+    num_2 = int(input("Digite outro número: "))
+    multiplicaçao = num_1 * num_2
+    print(f"O resultado da multiplicação é {multiplicaçao}")
+except:
+    print("Insira um número inteiro")
+
+# 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
+try:
+    numero_1 = int(input( "Digite um numero inteiro:"))
+    numero_2 = int(input("Digite outro numero inteiro:"))
+    divisao_inteira = numero_1 // numero_2
+    print(f"A divisão de {numero_1} por {numero_2} é igual a {divisao_inteira}")
+except:
+    print("Não é possível realizar essa operação!")
 
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+try:
+    numero = int(input("Digite um numero: "))
+    numero_quadrado = numero ** 2
+    print(f"O número {numero} ao quadrado é {numero_quadrado}")
+except:
+    print("Insira um número inteiro")
 
 # #### Números de Ponto Flutuante (`float`)
 # 6. Escreva um programa que receba dois números flutuantes e realize sua adição.
@@ -35,11 +62,11 @@ import math
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
-data = str(input("Digite uma data no modelo dd/mm/aaaa: "))
-dia, mes, ano = data.split("/")
-print(f"Dia é {dia}")
-print (f"Mês é {mes}")
-print (f"Ano é {ano}")
+#data = str(input("Digite uma data no modelo dd/mm/aaaa: "))
+#dia, mes, ano = data.split("/")
+#print(f"Dia é {dia}")
+#print (f"Mês é {mes}")
+#print (f"Ano é {ano}")
 
 
 
